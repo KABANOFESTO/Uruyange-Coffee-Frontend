83614509c6e44cd4d9c7387881d1cb077e58cb33
KABANOFESTO <kabanofesto1@gmail.com>
2025-03-28 15:22:27 +0200

    ft_sub_fix

--- src/lib/redux/slices/subscribersSlice.ts
+++ src/lib/redux/slices/subscribersSlice.ts
@@ -1,17 +1,14 @@
 import { apiSlice } from "./ApiSlice";
-
 const subscriptionApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         // Fetch all subscriptions
         subscriptions: builder.query({
             query: () => ({ url: "/subscriptions" }), // Relative to baseUrl
         }),
-
         // Fetch a single subscription by ID
         singleSubscription: builder.query({
             query: (id) => ({ url: `/subscriptions/${id}` }), // Relative to baseUrl
         }),
-
         // Create a new subscription
         createSubscription: builder.mutation({
             query: (data) => ({
@@ -20,7 +17,6 @@ const subscriptionApi = apiSlice.injectEndpoints({
                 body: data,
             }),
         }),
-
         // Delete a subscription by ID
         deleteSubscription: builder.mutation({
             query: (id) => ({
@@ -28,22 +24,33 @@ const subscriptionApi = apiSlice.injectEndpoints({
                 method: "DELETE",
             }),
         }),
-
         // Update a subscription by ID
         updateSubscription: builder.mutation({
             query: ({ id, data }) => ({
-                url: `/subscriptions/${id}`, 
+                url: `/subscriptions/${id}`,
                 method: "PUT",
                 body: data,
             }),
         }),
+        // Get bought subscriptions for a user
+        boughtSubscription: builder.query({
+            query: (params) => ({
+                url: "/suscriptionUser",
+                params: {
+                    page: params?.page || 1,
+                    limit: params?.limit || 10,
+                    search: params?.search || ''
+                }
+            }),
+            // Remove transformResponse or adjust it if you need to transform the data
+        }),
     }),
 });
-
 export const {
     useSubscriptionsQuery,
     useSingleSubscriptionQuery,
     useCreateSubscriptionMutation,
     useDeleteSubscriptionMutation,
     useUpdateSubscriptionMutation,
+    useBoughtSubscriptionQuery, // New query hook added
 } = subscriptionApi;
